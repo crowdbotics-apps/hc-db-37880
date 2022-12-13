5ac53b7ad2a1b3a202eb594c75758a8d6fa08133
crowdbotics-apps <team@crowdbotics.com>
2022-12-13 14:58:12 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ProviderPractices
+admin.site.register(ProviderPractices)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class ProviderPractices(models.Model):
+    'Generated Model'
+    practice = models.TextField()
+    state = models.TextField()
+    website = models.URLField()
+    num_org_mem = models.IntegerField()
